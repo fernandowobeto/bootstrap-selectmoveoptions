a7ba6e42ada0430366a96fd0ea49f92aa0b5d355
Fernando Wobeto <fernandowobeto@gmail.com>
2013-04-30 23:09:56 -0300

    modificações no plugin

--- bootstrap-selectmoveoptions.js
+++ bootstrap-selectmoveoptions.js
@@ -1,7 +1,7 @@
 (function($){
-	$.fn.select_move_options = function(){
+	$.fn.selectMoveOptions = function(){
 		$(this).each(function(){
-			var el		= $(this);
+			var el		= $(this).css('overflow','auto');
 			var selects = el.find('select');
 			var select1 = selects.eq(0).addClass('pull-left').css({'width':'45%','min-height':'120px','height':'auto'});
 			var select2 = selects.eq(1).addClass('pull-right').css({'width':'45%','min-height':'120px','height':'auto'});
@@ -16,12 +16,12 @@
 				option.remove();
 				select1.append(option);
 			});
-			var divControl			= $('<div>').css({'margin-left':'auto','margin-right':'auto','width':'10%','text-align':'center'});
-			var divButtonGroup	= $('<div>').addClass('btn-group btn-group-vertical').appendTo(divControl);
-			var icon_all_left		= $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-fast-backward'));
-			var icon_left			= $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-backward'));
-			var icon_right			= $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-forward'));
-			var icon_all_right	= $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-fast-forward'));
+			var divControl			 = $('<div>').css({'margin-left':'auto','margin-right':'auto','width':'10%','text-align':'center'});
+			var divButtonGroup	 = $('<div>').addClass('btn-group btn-group-vertical').appendTo(divControl);
+			var icon_all_left		 = $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-fast-backward'));
+			var icon_left				 = $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-backward'));
+			var icon_right			 = $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-forward'));
+			var icon_all_right	 = $('<button>').attr('type','button').addClass('btn').append($('<i>').addClass('icon-fast-forward'));
 
 			icon_left.click(function(){
 				var selecteds = select2.find('option:selected');
